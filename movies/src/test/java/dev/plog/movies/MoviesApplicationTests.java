67e3dae4e009ae13a6338ca7570b1fb6d99f4c7c
Matt Plog <MPlogSWDev@outlook.com>
2024-02-07 01:02:14 -0600

    Initial commit

---
+++ movies/src/test/java/dev/plog/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.plog.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
